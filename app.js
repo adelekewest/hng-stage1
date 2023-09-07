94c736daba810f62c82c3356cfc757e6d62d7feb
adelekewest <adelekeolugbenga4@gmail.com>
2023-09-07 11:15:36 +0100

    Adjusted project

--- app.js
+++ app.js
@@ -7,22 +7,25 @@ const dayOfWeek = currentDate.getDay();
 const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const currentDay = daysOfWeek[dayOfWeek];
 
-const year = currentDate.getUTCFullYear();
-const month = (currentDate.getUTCMonth() + 1).toString().padStart(2, '0'); // Months are zero-based
-const day = currentDate.getUTCDate().toString().padStart(2, '0');
-const hour = currentDate.getUTCHours().toString().padStart(2, '0');
-
 // Generate a random number between -2 and 2 (representing minutes)
 const randomMinutes = Math.floor(Math.random() * 5) - 2;
 
 // Apply the random adjustment to the current time
 currentDate.setUTCMinutes(currentDate.getUTCMinutes() + randomMinutes);
 
-// Format the adjusted UTC time as a string
-const formattedMinutes = currentDate.toISOString().slice(0, 19) + 'Z';
+// Get the individual components (year, month, day, hour, minute, second)
+const year = currentDate.getUTCFullYear();
+const month = (currentDate.getUTCMonth() + 1).toString().padStart(2, '0'); // Months are zero-based
+const day = currentDate.getUTCDate().toString().padStart(2, '0');
+const hour = currentDate.getUTCHours().toString().padStart(2, '0');
+const minute = currentDate.getUTCMinutes().toString().padStart(2, '0');
+const second = currentDate.getUTCSeconds().toString().padStart(2, '0');
 
 // Create the formatted UTC time string
-const formattedUTC = `${year}-${month}-${day}T${hour}:${formattedMinutes}`;
+const formattedUTC = `${year}-${month}-${day}T${hour}:${minute}:${second}Z`;
+
+
+
 
 
 app.get("/api", (req, res) => {
