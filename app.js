770f7f2bb32cd697fc811f6baba568e00c52a4e2
adelekewest <adelekeolugbenga4@gmail.com>
2023-09-08 19:29:51 +0100

    Updated code

--- app.js
+++ app.js
@@ -9,17 +9,17 @@ const currentDay = daysOfWeek[dayOfWeek];
 
 
 // Get the individual components (year, month, day, hour, minute, second)
-const year = currentDate.getUTCFullYear();
-const month = String(currentDate.getUTCMonth() + 1).padStart(2, "0");
-const day = String(currentDate.getUTCDate()).padStart(2, "0");
-const hour = String(currentDate.getUTCHours()).padStart(2, "0");
-const minute = String(currentDate.getUTCMinutes()).padStart(2, "0");
-const second = String(currentDate.getUTCSeconds()).padStart(2, "0");
+//const year = currentDate.getUTCFullYear();
+//const month = String(currentDate.getUTCMonth() + 1).padStart(2, "0");
+//const day = String(currentDate.getUTCDate()).padStart(2, "0");
+//const hour = String(currentDate.getUTCHours()).padStart(2, "0");
+//const minute = String(currentDate.getUTCMinutes()).padStart(2, "0");
+//const second = String(currentDate.getUTCSeconds()).padStart(2, "0");
 
 // Create the formatted UTC time string
-const formattedUTC = `${year}-${month}-${day}T${hour}:${minute}:${second}Z`;
+//const formattedUTC = `${year}-${month}-${day}T${hour}:${minute}:${second}Z`;
 
-
+const utcString = currentDate.toISOString().replace(/.\d+Z$/g, "Z");
 
 
 
@@ -27,7 +27,7 @@ app.get("/api", (req, res) => {
     return res.json({
         "slack_name": req.query.slack_name,
         "current_day": currentDay,
-        "utc_time": formattedUTC,
+        "utc_time": utcString,
         "track": req.query.track,
         "github_file_url": "https://github.com/adelekewest/hng-stage1/app.js",
         "github_repo_url": "https://github.com/adelekewest/hng-stage1",
