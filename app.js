7bd9c195792b2cf6042b73884ba85a923fde2397
adelekewest <adelekeolugbenga4@gmail.com>
2023-09-08 18:31:18 +0100

    Updated code

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ const currentDay = daysOfWeek[dayOfWeek];
 
 // Get the individual components (year, month, day, hour, minute, second)
 const year = currentDate.getUTCFullYear();
-const month = (currentDate.getUTCMonth() + 1).toString().padStart(2, '0'); // Months are zero-based
-const day = currentDate.getUTCDate().toString().padStart(2, '0');
-const hour = currentDate.getUTCHours().toString().padStart(2, '0');
-const minute = currentDate.getUTCMinutes().toString().padStart(2, '0');
-const second = currentDate.getUTCSeconds().toString().padStart(2, '0');
+const month = String(currentDate.getUTCMonth() + 1).padStart(2, "0");
+const day = String(currentDate.getUTCDate()).padStart(2, "0");
+const hour = String(currentDate.getUTCHours()).padStart(2, "0");
+const minute = String(currentDate.getUTCMinutes()).padStart(2, "0");
+const second = String(currentDate.getUTCSeconds()).padStart(2, "0");
 
 // Create the formatted UTC time string
 const formattedUTC = `${year}-${month}-${day}T${hour}:${minute}:${second}Z`;
